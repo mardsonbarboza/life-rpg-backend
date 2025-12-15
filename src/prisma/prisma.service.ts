6fb30af8111ff6b936ad23bfc122f6fef7ab35a8
mardson barboza <mardson49@gmail.com>
2025-12-15 18:27:34 -0300

    chore: downgrade Prisma dependencies to version 6.19.0 and update schema configuration
    - Updated package.json to downgrade "@prisma/client" and "prisma" to version 6.19.0.
    - Modified prisma/schema.prisma to change the client provider and output path.
    - Refactored PrismaService in src/prisma/prisma.service.ts to remove unnecessary constructor and logging.
    - Added an empty body to UsersController in src/users/users.controller.ts.
    - Created a new .vscode/settings.json file to pin to Prisma 6.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,24 +1,17 @@
+// src/prisma/prisma.service.ts
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  constructor() {
-    super({
-      datasourceUrl: process.env.DATABASE_URL,
-    });
-  }
-
   async onModuleInit() {
     await this.$connect();
-    console.log('Prisma connected to the database');
   }
+
   async onModuleDestroy() {
     await this.$disconnect();
-    console.log('Prisma disconnected from the database');
   }
 }
